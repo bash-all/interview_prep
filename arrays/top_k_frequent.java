75ce1640805f700d49f3edc59b3bf2f611d33328
Nisha Prakash <91690977+bash-all@users.noreply.github.com>
2023-06-26 19:00:17 +0530

    Update top_k_frequent.java

--- arrays/top_k_frequent.java
+++ arrays/top_k_frequent.java
@@ -3,6 +3,29 @@ import java.util.*;
 public class top_k_frequent {
     public static List<Integer> topKFrequent(int[] nums, int k) {
 
+        Map<Integer,Integer> map=new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put( nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        
+        PriorityQueue<int[]> pq=new PriorityQueue<>( (a,b)->b[1]-a[1]);
+        
+        for(int i:map.keySet())
+        {
+            pq.offer(new int[]{i,map.get(i)});
+        }
+        
+        int[] ans=new int[k];
+        while(k>0)
+        {
+            ans[--k]=pq.remove()[0];
+        }
+        
+        
+        return ans;
+
       
     }
 
