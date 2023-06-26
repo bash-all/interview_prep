3c4997f0957575f59ff0fc52da6fa66a19564e96
Nisha Prakash <91690977+bash-all@users.noreply.github.com>
2023-06-26 18:58:36 +0530

    Add files via upload

---
+++ arrays/top_k_frequent.java
@@ -0,0 +1,14 @@
+package array;
+import java.util.*;
+public class top_k_frequent {
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+
+      
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(topKFrequent(new int[]{1,1,1,2,2,3},2));
+    }
+    
+}
